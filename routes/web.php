056ca6d20c891e6ff3070b7b44a49a01f4d02a70
firmanchatomz <147006136@student.unsil.ac.id>
2021-02-23 22:04:11 +0700

    membersihkan file yang tidak digunakan

--- routes/web.php
+++ routes/web.php
@@ -36,15 +36,4 @@ Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
 
     Route::get('member', Members::class)->name('member'); //Tambahkan routing ini
     Route::resource('datapokok', 'App\Http\Controllers\Bumdes\DatapokokController');
-    Route::resource('unit', 'App\Http\Controllers\Bumdes\UnitController');
-    Route::resource('laporan', 'App\Http\Controllers\Bumdes\LaporanController');
-    Route::resource('jurnalumum', 'App\Http\Controllers\Bumdes\JurnalumumController');
-    Route::resource('daftarakun', 'App\Http\Controllers\Bumdes\DaftarakunController');
-    Route::resource('daftarakunpembantu', 'App\Http\Controllers\Bumdes\DaftarakunpembantuController');
-    Route::post('bumdes/daftarakun/import','App\Http\Controllers\Bumdes\DaftarakunController@import');
-    Route::get('bumdes/jurnal/{sesi}','App\Http\Controllers\Bumdes\JurnalumumController@lihatjurnal');
-    // laporan
-    Route::get('laporankeuangan/{sesi}','App\Http\Controllers\Bumdes\LaporankeuanganController@laporan');
-    Route::get('laporanbukubesar/{id}','App\Http\Controllers\Bumdes\LaporankeuanganController@bukubesar');
-    Route::post('laporanbukubesar','App\Http\Controllers\Bumdes\LaporankeuanganController@bukubesarpost');
 });
