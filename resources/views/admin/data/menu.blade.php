a080552c252345a7a759fddb60dacbde93d00000
firmanchatomz <147006136@student.unsil.ac.id>
2021-03-10 17:29:55 +0700

    update menu admin

--- resources/views/admin/data/menu.blade.php
+++ resources/views/admin/data/menu.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-copy"></i>
+      <i class="nav-icon fas fa-heartbeat"></i>
       <p>
         Siaga Covid-19
         <i class="fas fa-angle-left right"></i>
@@ -10,13 +10,13 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="pages/layout/top-nav.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
           <p>Pendataan</p>
         </a>
       </li>
       <li class="nav-item">
         <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
           <p>Pemantauan</p>
         </a>
       </li>
@@ -24,7 +24,7 @@
 </li>
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-copy"></i>
+      <i class="nav-icon fas fa-landmark"></i>
       <p>
         Info Desa
         <i class="fas fa-angle-left right"></i>
@@ -33,22 +33,454 @@
     </a>
     <ul class="nav nav-treeview">
       <li class="nav-item">
-        <a href="{{ url('/profil')}}" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
+        <a href="{{ url('/view/profil')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-id-card nav-icon"></i>
           <p>Identitas Desa</p>
         </a>
       </li>
       <li class="nav-item">
         <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="far fa-map nav-icon"></i>
           <p>Wilayah Administratif</p>
         </a>
       </li>
       <li class="nav-item">
         <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
-          <i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="far fa-building nav-icon"></i>
           <p>Pemerintahan Desa</p>
         </a>
       </li>
     </ul>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-users"></i>
+      <p>
+        Kependudukan
+        <i class="fas fa-angle-left right"></i>
+        {{-- <span class="badge badge-info right">6</span> --}}
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{ url('/view/penduduk')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-id-badge nav-icon"></i>
+          <p>Penduduk</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="{{ url('/view/keluarga')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-address-card nav-icon"></i>
+          <p>Keluarga</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Rumah Tangga</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Kelompok</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Data Suplemen</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Calon Pemilih</p>
+        </a>
+      </li>
+    </ul>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-chart-line"></i>
+      <p>
+        Statistik
+        <i class="fas fa-angle-left right"></i>
+        {{-- <span class="badge badge-info right">6</span> --}}
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{ url('/')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="fas fa-chart-area nav-icon"></i>
+          <p>Statistik Kependudukan</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Laporan Bulanan</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Laporan Kelompok Rentan</p>
+        </a>
+      </li>
+    </ul>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-archive"></i>
+      <p>
+        Sekretariat
+        <i class="fas fa-angle-left right"></i>
+        {{-- <span class="badge badge-info right">6</span> --}}
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{ url('/')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Surat Masuk</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Surat Keluar</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Produk Hukum</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Informasi Publik</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Inventaris</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Klarifikasi Surat</p>
+        </a>
+      </li>
+    </ul>
+</li>
+<li class="nav-item">
+    <a href="#" class="nav-link">
+      <i class="nav-icon fas fa-envelope-open"></i>
+      <p>
+        Layanan Surat
+        <i class="fas fa-angle-left right"></i>
+        {{-- <span class="badge badge-info right">6</span> --}}
+      </p>
+    </a>
+    <ul class="nav nav-treeview">
+      <li class="nav-item">
+        <a href="{{ url('/')}}" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Pengaturan Surat</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Cetak Surat</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Arsip Layanan</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Panduan</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          <p>Daftar Persyaratan</p>
+        </a>
+      </li>
+    </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-chart-bar"></i>
+    <p class="text">Analisis</p>
+  </a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-wallet"></i>
+    <p>
+      Keuangan
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Import Data</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Laporan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Input Data</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Laporan Manual</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Keuangan Harian</p>
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-people-carry"></i>
+    <p class="text">Bantuan</p>
+  </a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-map-signs"></i>
+    <p class="text">Pertanahan</p>
+  </a>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-map-marked-alt"></i>
+    <p>
+      Pemetaan
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Peta</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pengaturan Peta</p>
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-envelope"></i>
+    <p>
+      SMS
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>SMS</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Daftar Kontak</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pengaturan SMS</p>
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-cog"></i>
+    <p>
+      Pengaturan
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Modul</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Aplikasi</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pengguna</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Database</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Info Sistem</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>QR Code</p>
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-desktop"></i>
+    <p>
+      Admin Web
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Artikel</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Widget</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Menu</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Komentar</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Galeri</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Media Social</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Slider</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Teks Berjalan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pengunjung</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pengaturan</p>
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="nav-item">
+  <a href="#" class="nav-link">
+    <i class="nav-icon fas fa-house-user"></i>
+    <p>
+      Layanan Mandiri
+      <i class="fas fa-angle-left right"></i>
+      {{-- <span class="badge badge-info right">6</span> --}}
+    </p>
+  </a>
+  <ul class="nav nav-treeview">
+    <li class="nav-item">
+      <a href="{{ url('/')}}" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Permohonan Surat</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Kotak Pesan</p>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+        <p>Pendaftar Layanan Mandiri</p>
+      </a>
+    </li>
+  </ul>
 </li>
