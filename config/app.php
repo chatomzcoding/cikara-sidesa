53f4862842d95c26e342bc56354449bb23f5af7d
chatomzcoding <coding.chatomz@gmail.com>
2021-03-10 19:53:19 +0700

    membuat provider untuk sidesa

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         App\Providers\JetstreamServiceProvider::class,
 
         App\Providers\CikaraServiceProvider::class,
+        App\Providers\SidesaServiceProvider::class,
 
     ],
 
