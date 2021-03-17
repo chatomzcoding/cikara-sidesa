fc35f8659ec099f0b4d86ff9d249536126b87261
chatomzcoding <coding.chatomz@gmail.com>
2021-03-17 08:25:46 +0700

    penambahan fitur anggota rumah tangga

---
+++ app/Http/Controllers/Sidesa/AnggotarumahtanggaController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Sidesa;
+
+use App\Http\Controllers\Controller;
+use App\Models\Anggotarumahtangga;
+use Illuminate\Http\Request;
+
+class AnggotarumahtanggaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Anggotarumahtangga::create($request->all());
+
+        return redirect()->back()->with('ds','Anggota Rumah Tangga');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Anggotarumahtangga  $anggotarumahtangga
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anggotarumahtangga $anggotarumahtangga)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Anggotarumahtangga  $anggotarumahtangga
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anggotarumahtangga $anggotarumahtangga)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Anggotarumahtangga  $anggotarumahtangga
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anggotarumahtangga $anggotarumahtangga)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Anggotarumahtangga  $anggotarumahtangga
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anggotarumahtangga $anggotarumahtangga)
+    {
+        $anggotarumahtangga->delete();
+
+        return redirect()->back()->with('dd','Anggota Rumah Tangga');
+    }
+}
