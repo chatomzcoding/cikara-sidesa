4970a4387078dbfae350cdc03fff2590a09282e7
chatomzcoding <coding.chatomz@gmail.com>
2021-03-17 08:26:03 +0700

    mendefinisikan menu rumah tangga

--- resources/views/admin/data/menu.blade.php
+++ resources/views/admin/data/menu.blade.php
@@ -75,7 +75,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+        <a href="{{ url('/rumahtangga')}}" class="nav-link">
           &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
           <p>Rumah Tangga</p>
         </a>
