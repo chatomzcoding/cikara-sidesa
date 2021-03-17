6629a1dd84956c992d7ea74be6095e8d5be788ac
chatomzcoding <coding.chatomz@gmail.com>
2021-03-17 10:46:14 +0700

    membuat controller suplemen

---
+++ app/Http/Controllers/Sidesa/SuplemenController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Sidesa;
+
+use App\Http\Controllers\Controller;
+use App\Models\Suplemen;
+use Illuminate\Http\Request;
+
+class SuplemenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Suplemen  $suplemen
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Suplemen $suplemen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Suplemen  $suplemen
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Suplemen $suplemen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Suplemen  $suplemen
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Suplemen $suplemen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Suplemen  $suplemen
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Suplemen $suplemen)
+    {
+        //
+    }
+}
