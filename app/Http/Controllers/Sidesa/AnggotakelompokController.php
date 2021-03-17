adf9206d25cbb720ed36fc0aeba00ad9284e3a25
chatomzcoding <coding.chatomz@gmail.com>
2021-03-17 09:56:53 +0700

    membuat controller anggota kelompok

---
+++ app/Http/Controllers/Sidesa/AnggotakelompokController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Sidesa;
+
+use App\Http\Controllers\Controller;
+use App\Models\Anggotakelompok;
+use Illuminate\Http\Request;
+
+class AnggotakelompokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Anggotakelompok  $anggotakelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anggotakelompok $anggotakelompok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Anggotakelompok  $anggotakelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anggotakelompok $anggotakelompok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Anggotakelompok  $anggotakelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anggotakelompok $anggotakelompok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Anggotakelompok  $anggotakelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anggotakelompok $anggotakelompok)
+    {
+        //
+    }
+}
