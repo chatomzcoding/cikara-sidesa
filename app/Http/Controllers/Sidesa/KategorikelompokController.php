4e9de5245e76cd483e16118301c7e1192e48ee28
chatomzcoding <coding.chatomz@gmail.com>
2021-03-17 08:32:39 +0700

    membuat controller kategori kelompok

---
+++ app/Http/Controllers/Sidesa/KategorikelompokController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Sidesa;
+
+use App\Http\Controllers\Controller;
+use App\Models\Kategorikelompok;
+use Illuminate\Http\Request;
+
+class KategorikelompokController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kategorikelompok  $kategorikelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kategorikelompok $kategorikelompok)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kategorikelompok  $kategorikelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kategorikelompok $kategorikelompok)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kategorikelompok  $kategorikelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kategorikelompok $kategorikelompok)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kategorikelompok  $kategorikelompok
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kategorikelompok $kategorikelompok)
+    {
+        //
+    }
+}
