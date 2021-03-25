e5e9914cc1bafa843dffea69392111d17340a048
chatomzcoding <coding.chatomz@gmail.com>
2021-03-25 15:09:25 +0700

    membuat data info dashboard 4 kolom, dusun, penduduk, keluarga dan bantuan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,13 +16,74 @@
           </div><!-- /.row -->
     </x-slot>
   
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{-- <x-jet-welcome /> --}}
+        <!-- Main content -->
+        <section class="content">
+          <div class="container-fluid">
+            <!-- Info boxes -->
+            <div class="row">
+              <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box">
+                  <span class="info-box-icon bg-info elevation-1"><i class="fas fa-map-marked"></i></span>
+    
+                  <div class="info-box-content">
+                    <span class="info-box-text">Wilayah Dusun</span>
+                    <span class="info-box-number">
+                      {{ DbCikara::countData('dusun')}}
+                      {{-- <small>%</small> --}}
+                    </span>
+                  </div>
+                  <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+              </div>
+              <!-- /.col -->
+              <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                  <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-user"></i></span>
+    
+                  <div class="info-box-content">
+                    <span class="info-box-text">Penduduk</span>
+                    <span class="info-box-number">{{ DbCikara::countData('penduduk')}}</span>
+                  </div>
+                  <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+              </div>
+              <!-- /.col -->
+    
+              <!-- fix for small devices only -->
+              <div class="clearfix hidden-md-up"></div>
+    
+              <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                  <span class="info-box-icon bg-success elevation-1"><i class="fas fa-users"></i></span>
+    
+                  <div class="info-box-content">
+                    <span class="info-box-text">Keluarga</span>
+                    <span class="info-box-number">{{ DbCikara::countData('keluarga')}}</span>
+                  </div>
+                  <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+              </div>
+              <!-- /.col -->
+              <div class="col-12 col-sm-6 col-md-3">
+                <div class="info-box mb-3">
+                  <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-people-carry"></i></span>
+    
+                  <div class="info-box-content">
+                    <span class="info-box-text">Bantuan</span>
+                    <span class="info-box-number">{{ DbCikara::countData('bantuan')}}</span>
+                  </div>
+                  <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+              </div>
+              <!-- /.col -->
             </div>
-        </div>
-    </div>
+            <!-- /.row -->
+          </div>
+        </section>
     <x-slot name="script">
     </x-slot>
 </x-app-layout>
