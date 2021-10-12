a8a9d6991ddfd3f9c231127a4e8ff9330d6f9d93
chatomzcoding <coding.chatomz@gmail.com>
2021-10-12 12:36:20 +0700

    percobaan api produk

---
+++ app/Http/Controllers/Api/ProdukController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Produk;
+use Illuminate\Http\Request;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Produk::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $produk = New Produk;
+        $produk->lapak_id = $request->lapak_id;
+        $produk->nama = $request->nama;
+        $produk->keterangan = $request->keterangan;
+        $produk->gambar = $request->gambar;
+        $produk->harga = $request->harga;
+
+        $produk->save();
+
+        if (response()) {
+            $result["success"] = "1";
+            $result["message"] = "success";
+        } else {
+            $result["success"] = "0";
+            $result["message"] = "error";
+        }
+
+        return json_encode($result);
+        
+
+    //     return response()->json('Program deleted successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Produk $produk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Produk  $produk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Produk $produk)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('produk','App\Http\Controllers\Api\ProdukController@index');
+Route::post('produk','App\Http\Controllers\Api\ProdukController@store');
