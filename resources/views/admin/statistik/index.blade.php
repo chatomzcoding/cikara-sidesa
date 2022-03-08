3a0c72105d2bbd8f387e71af0a2e90e902e7a48c
chatomzcoding <coding.chatomz@gmail.com>
2022-03-08 15:34:02 +0700

    ubah type breadcrum

--- resources/views/admin/statistik/index.blade.php
+++ resources/views/admin/statistik/index.blade.php
@@ -11,7 +11,7 @@
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="{{ route('dashboard')}}">Beranda</a></li>
-          <li class="breadcrumb-item active">Stattistik Kependudukan</li>
+          <li class="breadcrumb-item active">Statistik Kependudukan</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
